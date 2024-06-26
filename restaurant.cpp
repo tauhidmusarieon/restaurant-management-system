df6c6526a33c3a7c01d917247e24c311a5609937
Tauhid Musa Rieon <tauhidmusarieon01@gmail.com>
2024-06-27 01:56:26 +0600

    adding some test code
    First time using  the unordered map . And I try to the just designed a normal login and exist part.

--- restaurant.cpp
+++ restaurant.cpp
@@ -1,12 +1,50 @@
-//#include <bits/stdc++.h>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
 
-    cout << "Journey starts from here";
+    unordered_map<string, string> ump;
 
+    ump["Rieon"] = "1032";
+    ump["Abir"] = "1026";
+    ump["Sazzad"] = "1005";
+    ump["Aronno"] = "1001";
+    ump["username"] = "1234";
+
+    while(true)
+    {
+        string option;
+        cout << "1. Login\n2. Exit\nEnter Option : ";
+        cin >> option ;
+
+        if(option == "1")
+        {
+            string username, password;
+            cout << "Enter Username : ";
+            cin >> username ;
+            cout << "Enter Password : ";
+            cin >> password ;
+
+            if(ump.find(username) != ump.end() && ump[username]==password)
+            {
+                cout << "Login successful" << endl;
+            }
+            else{
+                cout << "Invalid Username or Password" << endl;
+            }
+        }
+        else if(option=="2")
+        {
+            cout << "Exiting..." << endl;
+            return 0;
+        }
+        else
+        {
+            cout << "Invalid option, please try again" << endl;
+
+        }
+    }
 
 
 
